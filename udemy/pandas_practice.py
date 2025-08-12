88a69f427b95924197c6b5ba6d64aa6b3b2f308c
Zach <zachnelson275@gmail.com>
2025-08-11 21:07:21 -0600

    pandas udemy

--- udemy/pandas_practice.py
+++ udemy/pandas_practice.py
@@ -40,8 +40,7 @@ print(df)
 df['new'] = df['W'] + df['Y']
 print(df)
 df.drop('new', axis = 1, inplace = True)
-print(df)
-df = df.drop('E')
+# df = df.drop('E')
 print(df)
 
 df['W'] # Grab a column
@@ -50,3 +49,17 @@ df.iloc[2] # Grab a row by index
 
 df.loc['B','Y'] # Grab an item, row,column
 df.loc[['A', 'B'], ['W', 'Y']] # Grab multiple items by range
+
+booldf = df > 0 # Returns bool data frame
+# df[booldf] == df[df>0]
+
+df['W'] > 0 # Returns series of bool values
+df[df['W'] > 0] # Returns data frame where bool values = True
+# In this case, returns all items from rows A, B, D, and E
+# This is because df['W'] > 0 filters out all items in column W that are less than 0
+# which is just row C. Then, passing that series into the original data frame
+# returns all rows except C
+# Column conditionals return a series values, which can be combined with row
+# conditionals to filter data even more
+
+print(df[df['W'] > 0][['X', 'Y']])
