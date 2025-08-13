dc792d0421acd9590d93f7dc7e252e3ee1568578
carlnelson275 <zachnelson275@gmail.com>
2025-08-13 15:43:13 -0600

    pandas concatenate, merge, join

--- udemy/pandas_practice.py
+++ udemy/pandas_practice.py
@@ -110,3 +110,31 @@ print(dft.dropna(subset = ['a'])) # Drops rows where column a has null values
 
 print(dft.fillna('FILL VALUE')) # Fills null values with parameter
 print(dft['a'].fillna(value=dft['a'].mean())) # Fills null values with mean of column A
+
+### Group By ###
+data = {'Company':['GOOG', 'GOOG', 'MSFT', 'MSFT', 'FB', 'FB'],
+        'Person':['Sam', 'Charlie', 'Amy', 'Vanessa', 'Carl', 'Sarah'],
+        'Sales':[200, 120, 340, 124, 243, 350]}
+dfg = p.DataFrame(data)
+byComp = dfg.groupby('Company') # Returns a groupby object
+byComp.mean(numeric_only=True) # Returns mean of each group
+
+
+byComp.sum(numeric_only=True).loc['FB'] # Returns sum of FB group
+byComp.describe() # Returns descriptive statistics of each group
+byComp.describe().transpose() # Transpose the output for better readability
+byComp.describe().transpose()['FB'] # Returns descriptive statistics of FB group
+
+### Concatenation ###
+df1 = p.DataFrame({'A': ['A0', 'A1', 'A2', 'A3'], 'B' : ['B0', 'B1', 'B2', 'B3'], 'C' : ['C0', 'C1', 'C2', 'C3'], 'D' : ['D0', 'D1', 'D2', 'D3']}, index = [0, 1, 2, 3])
+df2 = p.DataFrame({'A': ['A4', 'A5', 'A6', 'A7'], 'B' : ['B4', 'B5', 'B6', 'B7'], 'C' : ['C4', 'C5', 'C6', 'C7'], 'D' : ['D4', 'D5', 'D6', 'D7']}, index = [4, 5, 6, 7])
+df3 = p.DataFrame({'A': ['A8', 'A9', 'A10', 'A11'], 'B' : ['B8', 'B9', 'B10', 'B11'], 'C' : ['C8', 'C9', 'C10', 'C11'], 'D' : ['D8', 'D9', 'D10', 'D11']}, index = [8, 9, 10, 11])
+print(p.concat([df1, df2, df3])) # By default, concat joins the rows together vertically
+print(p.concat([df1, df2, df3], axis=1)) # Concatenates the dataframes horizontally. This case results in a 12x12 data frame
+
+left = p.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'], 'A': ['A0', 'A1', 'A2', 'A3'], 'B': ['B0', 'B1', 'B2', 'B3']})
+right = p.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'], 'C': ['C0', 'C1', 'C2', 'C3'], 'D': ['D0', 'D1', 'D2', 'D3']})
+merged = p.merge(left, right, how='inner', on='key') # Inner is default merge method
+print(merged)
+
+# Join is the same thing as merge, except it uses the index rather than a key column
