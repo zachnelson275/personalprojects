1fc2e37c264ba45b870755608809a11d5a88135e
Zach <zachnelson275@gmail.com>
2025-09-01 13:33:07 -0600

    matrix & grids

--- udemy/seaborn_practice.py
+++ udemy/seaborn_practice.py
@@ -1,10 +1,14 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as spy
 
 tips = sns.load_dataset('tips')
+flights = sns.load_dataset('flights')
+iris = sns.load_dataset('iris')
 
 ## Distribution Plots
+
 # print(tips.head())
 # sns.distplot(tips['total_bill'], kde = False, bins = 30)
 # KDE stands for Kernel Density Estimation
@@ -16,6 +20,7 @@ tips = sns.load_dataset('tips')
 
 
 ## Categorical Plots
+
 # sns.barplot(x = 'sex', y = 'total_bill', data = tips, estimator = np.std)
 # Visualize the group by action
 
@@ -29,5 +34,26 @@ tips = sns.load_dataset('tips')
 
 
 ## Matrix Plots
+
+tc = tips.corr(numeric_only=True)
+# sns.heatmap(tc, annot = True, cmap = 'coolwarm')
+fp = flights.pivot_table(index = 'month', columns = 'year', values = 'passengers')
+# sns.heatmap(fp, cmap = 'magma', linecolor = 'white', linewidth = 3)
+# sns.clustermap(fp, cmap = 'coolwarm', standard_scale = 1)
+# Clustermap groups like columns and rows together based on similar values
+
+
+## Grids
+
+# sns.pairplot(iris)
+# g = sns.PairGrid(iris)
+
+# g.map_diag(sns.distplot)
+# g.map_upper(plt.scatter)
+# g.map_lower(sns.kdeplot, cmap = 'cool_d')
+
+g = sns.FacetGrid(tips, col = 'time', row = 'smoker')
+
+g.map(plt.scatter, 'total_bill', 'tip')
 
 plt.show()
